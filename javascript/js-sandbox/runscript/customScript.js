fc689a68b64945b6bfde5d24e21521d6981d4009
philia897 <wangzekun.felix@gmail.com>
2022-05-30 23:34:34 +0200

    improve custom test script in js-sandbox to iterate all properties

--- javascript/js-sandbox/runscript/customScript.js
+++ javascript/js-sandbox/runscript/customScript.js
@@ -1,1 +1,14 @@
-obj.valueOf();
+// obj.valueOf();
+
+function test (obj) {
+    for(key in obj){
+        if(typeof obj[key] === 'object'){
+            test(obj[key]);
+        } else if (typeof obj[key] === 'function'){
+            var fun = (obj[key])();
+            fun();
+        } else {
+            var x = obj[key];
+        }
+    }
+}
